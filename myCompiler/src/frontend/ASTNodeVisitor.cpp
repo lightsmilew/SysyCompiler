b9b97dbb17c1d65ef8cc82a7d1f4bf47d40c7093
lightsmile <1529182157@qq.com>
2025-06-05 16:50:24 +0800

    增加了部分visitor函数

--- myCompiler/src/frontend/ASTNodeVisitor.cpp
+++ myCompiler/src/frontend/ASTNodeVisitor.cpp
@@ -110,3 +110,104 @@ antlrcpp::Any ASTNodeVisitor::visitConstInitList(SysYParser::ConstInitListContex
     }
     return initVals; // 返回一个包含所有初始化值的向量
 }
+antlrcpp::Any ASTNodeVisitor::visitVarDecl(SysYParser::VarDeclContext *ctx) {
+    //获取变量的类型
+    PrimaryDataType type = convertToPrimaryDataType(ctx->bType()->getText());
+    //存储所有变量的声明
+    auto decls=Vector<Ptr<ast::DeclStmtNode>>();
+    for(auto varDefs:ctx->varDef()){
+        String identifier;
+        Vector<Ptr<ast::ExprNode>> arrayIndices;
+        Ptr<ast::InitExprNode> initExprPtr = nullptr; // 初始化为 nullptr
+        //如果有初始化列表
+    if(varDefs->initVal()){
+        identifier = varDefs->ident()->getText();
+        //每一维的大小
+        for(auto constExpCtx:varDefs->constExp()){
+            arrayIndices.emplace_back(AS(visit(constExpCtx), Ptr<ast::ExprNode>));
+        }
+        auto initVal=varDefs->initVal()->accept(this);
+        if(initVal.IS(Ptr<ast::InitExprNode>)){
+            initExprPtr = AS(initVal, Ptr<ast::InitExprNode>);
+        } else if(initVal.IS(Vector<Ptr<ast::InitExprNode>>)){
+            //多维数组的初始化
+            auto initVals = AS(initVal, Vector<Ptr<ast::InitExprNode>>);
+            initExprPtr = makePtr<ast::InitExprNode>(initVals);
+        }
+    }else{
+        identifier = varDefs->ident()->getText();
+        for(auto constExpCtx:varDefs->constExp()){
+            arrayIndices.emplace_back(AS(visit(constExpCtx), Ptr<ast::ExprNode>));
+        }
+        initExprPtr=NULL;
+    }
+    DataType varType(PrimaryDataType);
+
+    //创建VarDecl 最后两个参数默认false
+    auto decl= makePtr<ast::DeclStmtNode>(varType, identifier, initExprPtr);
+    decl->indices=arrayIndices;
+    decls.emplace_back(decl);
+    }
+    return decls; // 返回所有变量声明的向量
+ }
+ antlrcpp::Any ASTNodeVisitor::visitInitExpr(SysYParser::InitExprContext *ctx) {
+    auto initVal=AS(ctx->exp()->accept(this), Ptr<ast::ExprNode>);
+    // 返回一个 InitExprNode，表示单一的初始化表达式
+    return makePtr<ast::InitExprNode>(initVal);
+ }
+antlrcpp::Any ASTNodeVisitor::visitInitList(SysYParser::InitListContext *ctx) {
+    Vector<Ptr<ast::InitExprNode>> initVals;
+    for (auto initValCtx : ctx->initVal()) {
+        // 访问每个 InitValContext，获取初始化值
+        auto initVal = visit(initValCtx);
+        if (initVal.IS(Ptr<ast::InitExprNode>)) {
+            initVals.emplace_back(AS(initVal, Ptr<ast::InitExprNode>));
+        } else if (initVal.IS(Vector<Ptr<ast::InitExprNode>>)) {
+            //多维数组嵌套，创建新的InitExprNode封装
+            auto vals = AS(initVal, Vector<Ptr<ast::InitExprNode>>);
+            initVals.emplace_back(makePtr<ast::InitExprNode>(vals));
+        }
+    }
+    return initVals; // 返回一个包含所有初始化值的向量
+}
+antlrcpp::Any ASTNodeVisitor::visitFuncDef(SysYParser::FuncDefContext *ctx) {
+    PrimaryDataType funcType=convertToPrimaryDataType(ctx->funcType()->getText());
+    DataType returnType(funcType);
+    // 获取函数名
+    String funcName = ctx->ident()->getText();
+    // 获取函数参数列表
+    Vector<Ptr<ast::DeclStmtNode>> params;
+    for (auto paramCtx : ctx->funcFParams()->funcFParam()) {
+        auto param = AS(paramCtx->accept(this), Ptr<ast::DeclStmtNode>);
+        params.emplace_back(param);
+    }
+    auto bodyptr=AS(visit(ctx->block()), Ptr<ast::BlockStmtNode>);
+    // 创建函数节点
+    auto funcNode = makePtr<ast::FuncNode>(returnType,funcName, params, bodyptr);
+    return funcNode; // 返回函数节点
+}
+antlrcpp::Any ASTNodeVisitor::visitTypeVoid(SysYParser::TypeVoidContext *ctx) {
+    return PrimaryDataType::VOID; // 返回 PrimaryDataType::VOID
+}
+antlrcpp::Any ASTNodeVisitor::visitTypeBType(SysYParser::TypeBTypeContext *ctx) {
+    return PrimaryDataType::INT; // 返回 PrimaryDataType::INT
+}
+//形参列表
+antlrcpp::Any ASTNodeVisitor::visitFuncFParams(SysYParser::FuncFParamsContext *ctx) {
+    Vector<Ptr<ast::DeclStmtNode>> params;
+    for (auto paramCtx : ctx->funcFParam()) {
+        auto param = AS(paramCtx->accept(this), Ptr<ast::DeclStmtNode>);
+        param->isFuncParam = true; // 设置为函数参数
+        params.emplace_back(param);
+    }
+    return params; // 返回函数参数列表
+}
+//处理实参列表
+antlrcpp::Any ASTNodeVisitor::visitFuncRParams(SysYParser::FuncRParamsContext *ctx) {
+    Vector<Ptr<ast::DeclStmtNode>> params;
+    for (auto paramCtx : ctx->exp()) {
+        auto param = AS(paramCtx->accept(this), Ptr<ast::ExprNode>);
+        params.emplace_back(param);
+    }
+    return params; // 返回函数参数列表
+}
