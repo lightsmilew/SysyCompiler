a766ec50a64db3fddaf798ac2acddd5602d7334c
100014946-Wu Runkang <2158493577@qq.com>
2025-06-12 10:40:15 +0800

    重构DataType类，移除函数相关信息并简化构造函数；在SemanticAnalysis.cpp中添加TypeCheckerVisitor的节点访问实现

--- myCompiler/src/frontend/SemanticAnalysis.cpp
+++ myCompiler/src/frontend/SemanticAnalysis.cpp
@@ -48,3 +48,63 @@ shared_ptr<Symbol> SemanticAnalyzer::resolveVariable(const std::string &name)
 {
   return currentScope->lookup(name);
 }
+
+// Implementation of visitCompUnit
+void TypeCheckerVisitor::visitCompUnit(shared_ptr<CompUnitNode> node)
+{
+  // Add the logic for visiting a CompUnitNode here
+}
+void TypeCheckerVisitor::visitFuncNode(shared_ptr<FuncNode> node)
+{
+}
+void TypeCheckerVisitor::visitBlockStmt(shared_ptr<BlockStmtNode> node)
+{
+}
+void TypeCheckerVisitor::visitDeclStmt(shared_ptr<DeclStmtNode> node)
+{
+}
+void TypeCheckerVisitor::visitExprStmt(shared_ptr<ExprStmtNode> node)
+{
+}
+void TypeCheckerVisitor::visitAssignStmt(shared_ptr<AssignStmtNode> node)
+{
+}
+void TypeCheckerVisitor::visitIfElseStmt(shared_ptr<IfElseStmtNode> node)
+{
+}
+void TypeCheckerVisitor::visitWhileStmt(shared_ptr<WhileStmtNode> node)
+{
+}
+void TypeCheckerVisitor::visitBreakStmt(shared_ptr<BreakStmtNode> node)
+{
+}
+void TypeCheckerVisitor::visitContinueStmt(shared_ptr<ContinueStmtNode> node)
+{
+}
+void TypeCheckerVisitor::visitReturnStmt(shared_ptr<ReturnStmtNode> node)
+{
+}
+void TypeCheckerVisitor::visitLValueExpr(shared_ptr<LValueExprNode> node)
+{
+}
+void TypeCheckerVisitor::visitInitExpr(shared_ptr<InitExprNode> node)
+{
+}
+void TypeCheckerVisitor::visitCallExpr(shared_ptr<CallExprNode> node)
+{
+}
+void TypeCheckerVisitor::visitBinaryExpr(shared_ptr<BinaryExprNode> node)
+{
+}
+void TypeCheckerVisitor::visitUnaryExpr(shared_ptr<UnaryExprNode> node)
+{
+}
+void TypeCheckerVisitor::visitLiteralExpr(shared_ptr<LiteralExprNode> node)
+{
+}
+void TypeCheckerVisitor::visitIntLiteralExpr(shared_ptr<IntLiteralExprNode> node)
+{
+}
+void TypeCheckerVisitor::visitFloatLiteralExpr(shared_ptr<FloatLiteralExprNode> node)
+{
+}
