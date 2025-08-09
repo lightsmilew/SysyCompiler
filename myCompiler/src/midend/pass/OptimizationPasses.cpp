49ed8f6c1268e9956874342523209eac86f62d85
lightsmile <1529182157@qq.com>
2025-08-09 14:08:27 +0800

    新增取模对非2的幂次方的强度优化削弱

--- myCompiler/src/midend/pass/OptimizationPasses.cpp
+++ myCompiler/src/midend/pass/OptimizationPasses.cpp
@@ -2122,19 +2122,19 @@ bool StrengthReductionPass::runOnFunction(Function *func)
                 Value *rhs = inst->getOperands()[1];
                 if (auto *constInt = dynamic_cast<ConstantInt *>(rhs))
                 {
-                    int val = constInt->Value;
-                    if (val > 0 && (val & (val - 1)) == 0)
+                    int d = constInt->Value;
+                    if (d > 0 && (d & (d - 1)) == 0)
                     {
                         // x % 2^n == ((x + bias) & mask) - bias
                         // bias = (x >> 31) & mask
-                        int n = 0, tmp = val;
+                        int n = 0, tmp = d;
                         while (tmp > 1)
                         {
                             tmp >>= 1;
                             n++;
                         }
                         auto *type = IntegerType::getInstance();
-                        int mask_val = val - 1;
+                        int mask_val = d - 1;
                         auto *mask_const = new ConstantInt(type, mask_val);
                         auto *shift31 = new ConstantInt(type, 31);
 
@@ -2162,8 +2162,52 @@ bool StrengthReductionPass::runOnFunction(Function *func)
                         changed = true;
                         if (verbose)
                         {
-                            debugInfo << "Strength Reduction: Replaced SRem with ((x+bias)&mask)-bias for " << val
+                            debugInfo << "Strength Reduction: Replaced SRem with ((x+bias)&mask)-bias for " << d
                                       << " in " << bb->getName() << "\n";
+                        }
+                    }
+                    if (d > 0 && (d & (d - 1)) != 0)
+                    {
+                        // 1. 计算magic和shift
+                        auto [magic, shift] = compute_magic(d);
+                        auto *type = IntegerType::getInstance();
+                        // 2. 扩展lhs为64位
+                        auto *lhs64 = new CastInst(Opcode::Sext, lhs, LongType::getInstance(), lhs->getName() + "_to64");
+                        // 3. 乘以magic
+                        auto *magic_const = new ConstantLong(LongType::getInstance(), magic);
+                        auto *mul = new BinaryOperator(Opcode::Muld, lhs64, magic_const, inst->getName() + "_mulmagic");
+                        // 4. 取高32位（算术右移32+shift位）
+                        auto *shiftnum = new ConstantLong(LongType::getInstance(), 32 + shift);
+                        auto *sra_div = new BinaryOperator(Opcode::Srad, mul, shiftnum, inst->getName() + "_sra_div");
+                        // 5. 截断回32位
+                        auto *q0 = new CastInst(Opcode::Trunc, sra_div, type, inst->getName() + "_divmagic");
+                        // 6. 修正：被除数为负时，结果加1
+                        auto *zero = new ConstantInt(type, 0);
+                        auto *sign = new ICmpInst(ICmpInst::ICMP_SLT, lhs, zero, inst->getName() + "_divsign");
+                        auto *q = new BinaryOperator(Opcode::Add, q0, sign, inst->getName() + "_divmagic_fix");
+                        // 7. rem = lhs - q * d
+                        auto *d_const = new ConstantInt(type, d);
+                        auto *q_mul_d = new BinaryOperator(Opcode::Mul, q, d_const, inst->getName() + "_qmul");
+                        auto *rem = new BinaryOperator(Opcode::Sub, lhs, q_mul_d, inst->getName() + "_remmagic");
+
+                        inst->replaceAllUsesWith(rem);
+                        inst->removeThisFromOperands();
+                        needToDelete.push_back(insts[i].release());
+                        insts.erase(insts.begin() + i);
+                        // 按顺序插入新指令
+                        insts.insert(insts.begin() + i, std::unique_ptr<Instruction>(rem));
+                        insts.insert(insts.begin() + i, std::unique_ptr<Instruction>(q_mul_d));
+                        insts.insert(insts.begin() + i, std::unique_ptr<Instruction>(q));
+                        insts.insert(insts.begin() + i, std::unique_ptr<Instruction>(sign));
+                        insts.insert(insts.begin() + i, std::unique_ptr<Instruction>(q0));
+                        insts.insert(insts.begin() + i, std::unique_ptr<Instruction>(sra_div));
+                        insts.insert(insts.begin() + i, std::unique_ptr<Instruction>(mul));
+                        insts.insert(insts.begin() + i, std::unique_ptr<Instruction>(lhs64));
+                        changed = true;
+                        if (verbose)
+                        {
+                            debugInfo << "Strength Reduction: Replaced SRem with magic number division for " << d
+                                      << " (magic=" << magic << ", shift=" << shift << ") in " << bb->getName() << "\n";
                         }
                     }
                 }
