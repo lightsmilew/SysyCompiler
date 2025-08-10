e68f73237c850b8e6307513857ac9c5732aeb5ce
lightsmile <1529182157@qq.com>
2025-08-10 12:48:47 +0800

    关闭ifconversion，无优化效果

--- myCompiler/src/midend/pass/OptimizationPasses.cpp
+++ myCompiler/src/midend/pass/OptimizationPasses.cpp
@@ -127,7 +127,7 @@ std::unique_ptr<PassManager> optimization::createOptimizationPipeline(Optimizati
         pm->addPass(std::make_unique<ConstantFoldingPass>(verbose));
         pm->addPass(std::make_unique<ModLoopReductionPass>(verbose));
         pm->addPass(std::make_unique<LoopUnrollingPass>(verbose));
-        pm->addPass(std::make_unique<IfConversionPass>(verbose));
+        //pm->addPass(std::make_unique<IfConversionPass>(verbose));
         pm->addPass(std::make_unique<BasicBlockMergePass>(verbose));
         pm->addPass(std::make_unique<DeadCodeEliminationPass>(verbose));
         pm->addPass(std::make_unique<GEPExpansionPass>(verbose));
@@ -205,7 +205,9 @@ std::unique_ptr<PassManager> optimization::createOptimizationPipeline(Optimizati
         // 进行循环展开后再来一次合并基本块
         pm->addPass(std::make_unique<LoopUnrollingPass>(verbose));
         // 消除简单ifelse
-        pm->addPass(std::make_unique<IfConversionPass>(verbose));
+        //pm->addPass(std::make_unique<IfConversionPass>(verbose));
+        // 
+        //
         pm->addPass(std::make_unique<BasicBlockMergePass>(verbose));
 
         pm->addPass(std::make_unique<DeadCodeEliminationPass>(verbose));
