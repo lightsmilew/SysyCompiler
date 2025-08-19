a047f0e2e92ecf4d1f70c2e38fc7d06d27c4b14a
lightsmile <1529182157@qq.com>
2025-08-19 14:37:55 +0800

    关闭尾递归消除

--- myCompiler/src/midend/pass/OptimizationPasses.cpp
+++ myCompiler/src/midend/pass/OptimizationPasses.cpp
@@ -128,8 +128,8 @@ std::unique_ptr<PassManager> optimization::createOptimizationPipeline(Optimizati
         pm->addPass(std::make_unique<CommonSubexpressionEliminationPass>(1, verbose));
         //pm->addPass(std::make_unique<RemoveRedundantStorePass>(verbose));
         pm->addPass(std::make_unique<FunctionInliningPass>(verbose));
-        //pm->addPass(std::make_unique<ArrayEliminationPass>(verbose));
-        //pm->addPass(std::make_unique<RemoveOnlyWriteArrayPass>(verbose));
+        pm->addPass(std::make_unique<ArrayEliminationPass>(verbose));
+        pm->addPass(std::make_unique<RemoveOnlyWriteArrayPass>(verbose));
         pm->addPass(std::make_unique<DeadCodeEliminationPass>(verbose));
         pm->addPass(std::make_unique<RemoveUselessWhilePass>(verbose));
         pm->addPass(std::make_unique<LoopSumReductionPass>(verbose));
@@ -142,7 +142,7 @@ std::unique_ptr<PassManager> optimization::createOptimizationPipeline(Optimizati
         pm->addPass(std::make_unique<DeadCodeEliminationPass>(verbose));
         pm->addPass(std::make_unique<GEPExpansionPass>(verbose));
         pm->addPass(std::make_unique<CommonSubexpressionEliminationPass>(verbose));
-        pm->addPass(std::make_unique<TailRecursionEliminationPass>(verbose));
+        //pm->addPass(std::make_unique<TailRecursionEliminationPass>(verbose));
         pm->addPass(std::make_unique<GEPToBitCastPass>(verbose));
         pm->addPass(std::make_unique<PhiEliminationPass>(verbose));
         pm->addPass(std::make_unique<AddChainReductionPass>(verbose));
