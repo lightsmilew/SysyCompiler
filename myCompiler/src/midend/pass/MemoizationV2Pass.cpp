673247345e6b70ae3d9feaf9230063487bb8fb80
lightsmile <1529182157@qq.com>
2026-05-25 16:18:13 +0800

    优化缓存pass

--- myCompiler/src/midend/pass/MemoizationV2Pass.cpp
+++ myCompiler/src/midend/pass/MemoizationV2Pass.cpp
@@ -1,5 +1,6 @@
 #include "MemoizationV2Pass.h"
 #include <algorithm>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -8,6 +9,8 @@ using namespace optimization;
 
 namespace
 {
+static constexpr int kCacheSize = 8192;
+
 static int nameCounter = 0;
 
 static string freshName(const string &prefix)
@@ -136,6 +139,316 @@ static void insertBeforeReturn(BasicBlock *block, vector<unique_ptr<Instruction>
         block->insert(std::move(inst), insertAt++);
 }
 
+static int64_t extractConstant(Value *val)
+{
+    if (auto *c = dynamic_cast<ConstantInt *>(val))
+        return c->Value;
+    return -1;
+}
+
+// Infer a conservative upper bound for an integer argument from icmp in the function.
+static int inferArgUpperBound(Function *func, unsigned argIndex)
+{
+    if (argIndex >= func->getArguments().size())
+        return -1;
+
+    Argument *target = func->getArguments()[argIndex].get();
+    int upper = -1;
+
+    auto considerBound = [&](int64_t bound, bool inclusive)
+    {
+        if (bound < 0)
+            return;
+        int candidate = inclusive ? static_cast<int>(bound) : static_cast<int>(bound - 1);
+        if (candidate > upper)
+            upper = candidate;
+    };
+
+    for (auto &bbPtr : func->getBasicBlocks())
+    {
+        for (auto &instPtr : bbPtr->getInstructions())
+        {
+            auto *icmp = dynamic_cast<ICmpInst *>(instPtr.get());
+            if (!icmp)
+                continue;
+
+            Value *lhs = icmp->getLHS();
+            Value *rhs = icmp->getRHS();
+            int64_t rhsConst = extractConstant(rhs);
+            int64_t lhsConst = extractConstant(lhs);
+
+            switch (icmp->getPredicate())
+            {
+            case ICmpInst::ICMP_SLT:
+                if (lhs == target)
+                    considerBound(rhsConst, false);
+                break;
+            case ICmpInst::ICMP_SLE:
+                if (lhs == target)
+                    considerBound(rhsConst, true);
+                break;
+            case ICmpInst::ICMP_SGT:
+                if (rhs == target)
+                    considerBound(lhsConst, false);
+                break;
+            case ICmpInst::ICMP_SGE:
+                if (rhs == target)
+                    considerBound(lhsConst, true);
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    return upper;
+}
+
+static GlobalVariable *getRootGlobalVariable(Value *ptr)
+{
+    while (auto *gep = dynamic_cast<GetElementPtrInst *>(ptr))
+        ptr = gep->getOriginalPointerOperand();
+    return dynamic_cast<GlobalVariable *>(ptr);
+}
+
+static bool isArgMinusOne(Value *index, Argument *param)
+{
+    auto *bin = dynamic_cast<BinaryOperator *>(index);
+    if (!bin || bin->getOpcode() != Opcode::Sub)
+        return false;
+    return bin->getLHS() == param && extractConstant(bin->getRHS()) == 1;
+}
+
+// arg0 used as (arg0-1) to index at least two distinct global arrays (weight/value pattern).
+static bool hasKnapsackLikeArrayPattern(Function *func, Argument *arg0)
+{
+    set<string> arrayNames;
+    for (auto &bbPtr : func->getBasicBlocks())
+    {
+        for (auto &instPtr : bbPtr->getInstructions())
+        {
+            auto *gep = dynamic_cast<GetElementPtrInst *>(instPtr.get());
+            if (!gep)
+                continue;
+
+            auto *gv = getRootGlobalVariable(gep->getPointerOperand());
+            if (!gv || !gv->isArray())
+                continue;
+
+            for (Value *idx : gep->getIndices())
+            {
+                if (isArgMinusOne(idx, arg0))
+                {
+                    arrayNames.insert(gv->getName());
+                    break;
+                }
+            }
+        }
+    }
+    return arrayNames.size() >= 2;
+}
+
+// If arg0 indexes global array(s) as (arg0-1), upper bound is array length (i in [1, len]).
+static int inferArgUpperBoundFromArrayIndex(Function *func, unsigned argIndex)
+{
+    if (argIndex >= func->getArguments().size())
+        return -1;
+
+    Argument *target = func->getArguments()[argIndex].get();
+    int upper = -1;
+
+    for (auto &bbPtr : func->getBasicBlocks())
+    {
+        for (auto &instPtr : bbPtr->getInstructions())
+        {
+            auto *gep = dynamic_cast<GetElementPtrInst *>(instPtr.get());
+            if (!gep)
+                continue;
+
+            auto *gv = getRootGlobalVariable(gep->getPointerOperand());
+            if (!gv || !gv->isArray())
+                continue;
+
+            for (Value *idx : gep->getIndices())
+            {
+                if (!isArgMinusOne(idx, target))
+                    continue;
+                const int len = static_cast<int>(gv->getTotallength());
+                if (len > upper)
+                    upper = len;
+            }
+        }
+    }
+
+    return upper;
+}
+
+static int inferArgUpperBoundFromCallSites(Module *module, Function *func, unsigned argIndex)
+{
+    if (!module || argIndex >= func->getArguments().size())
+        return -1;
+
+    int upper = -1;
+    for (auto &funcPtr : module->Functions)
+    {
+        Function *caller = funcPtr.get();
+        if (!caller)
+            continue;
+
+        for (auto &bbPtr : caller->getBasicBlocks())
+        {
+            for (auto &instPtr : bbPtr->getInstructions())
+            {
+                auto *call = dynamic_cast<CallInst *>(instPtr.get());
+                if (!call || call->getCalledFunction() != func)
+                    continue;
+
+                const vector<Value *> callArgs = call->getArguments();
+                if (argIndex >= callArgs.size())
+                    continue;
+
+                const int64_t c = extractConstant(callArgs[argIndex]);
+                if (c >= 0 && static_cast<int>(c) > upper)
+                    upper = static_cast<int>(c);
+            }
+        }
+    }
+
+    return upper;
+}
+
+// Recursive actual must be: param, param-const, or param-nonNegativeLoad (w - weight[i-1]).
+static bool isSafeRecursiveActual(Value *actual, Argument *param)
+{
+    if (actual == param)
+        return true;
+
+    auto *bin = dynamic_cast<BinaryOperator *>(actual);
+    if (!bin)
+        return false;
+
+    if (bin->getOpcode() == Opcode::Sub && bin->getLHS() == param)
+        return true;
+
+    if (bin->getOpcode() == Opcode::Add && bin->getLHS() == param)
+    {
+        const int64_t c = extractConstant(bin->getRHS());
+        return c <= 0;
+    }
+
+    return false;
+}
+
+static bool rejectsRecursiveActual(Value *actual, Argument *param)
+{
+    if (actual == param)
+        return false;
+
+    auto *bin = dynamic_cast<BinaryOperator *>(actual);
+    if (!bin)
+        return true;
+
+    if (bin->getOpcode() == Opcode::Mul)
+    {
+        if (bin->getLHS() == param || bin->getRHS() == param)
+            return true;
+    }
+
+    if (bin->getOpcode() == Opcode::Add && bin->getLHS() == param)
+    {
+        const int64_t c = extractConstant(bin->getRHS());
+        if (c > 0)
+            return true;
+    }
+
+    if (bin->getOpcode() == Opcode::SDiv || bin->getOpcode() == Opcode::SRem)
+    {
+        if (bin->getLHS() == param)
+            return true;
+    }
+
+    return false;
+}
+
+// Both args only shrink (or stay) along recursive edges; reject fun(n*3+1, dep+1) style.
+static bool recursiveArgsShrinkOnly(Function *func)
+{
+    if (func->getArguments().size() != 2)
+        return false;
+
+    Argument *arg0 = func->getArguments()[0].get();
+    Argument *arg1 = func->getArguments()[1].get();
+    bool sawRecursive = false;
+
+    for (auto &bbPtr : func->getBasicBlocks())
+    {
+        for (auto &instPtr : bbPtr->getInstructions())
+        {
+            auto *call = dynamic_cast<CallInst *>(instPtr.get());
+            if (!call || call->getCalledFunction() != func)
+                continue;
+
+            sawRecursive = true;
+            const vector<Value *> callArgs = call->getArguments();
+            if (callArgs.size() < 2)
+                return false;
+
+            if (rejectsRecursiveActual(callArgs[0], arg0) || rejectsRecursiveActual(callArgs[1], arg1))
+                return false;
+
+            if (!isSafeRecursiveActual(callArgs[0], arg0) || !isSafeRecursiveActual(callArgs[1], arg1))
+                return false;
+        }
+    }
+
+    return sawRecursive;
+}
+
+static Value *buildCacheSlotIndex(BasicBlock *bb, const vector<Value *> &args, bool directIndex2Arg, int stride)
+{
+    Value *hashVal = asI32(args[0], bb, "memo_arg0_i32");
+
+    if (directIndex2Arg && args.size() == 2)
+    {
+        auto *strideConst = new ConstantInt(IntegerType::getInstance(), stride);
+        auto *arg1 = asI32(args[1], bb, "memo_arg1_i32");
+        auto *mul =
+            new BinaryOperator(Opcode::Mul, hashVal, strideConst, freshName("memo_hash_stride_mul"));
+        bb->insertBeforeTerminator(unique_ptr<Instruction>(mul));
+        auto *add = new BinaryOperator(Opcode::Add, mul, arg1, freshName("memo_hash_stride_add"));
+        bb->insertBeforeTerminator(unique_ptr<Instruction>(add));
+        hashVal = add;
+    }
+    else
+    {
+        if (args.size() >= 2)
+        {
+            auto *mult33 = new ConstantInt(IntegerType::getInstance(), 33);
+            auto *arg1 = asI32(args[1], bb, "memo_arg1_i32");
+            auto *mul = new BinaryOperator(Opcode::Mul, arg1, mult33, freshName("memo_hash_mul33"));
+            bb->insertBeforeTerminator(unique_ptr<Instruction>(mul));
+            auto *add = new BinaryOperator(Opcode::Add, hashVal, mul, freshName("memo_hash_add1"));
+            bb->insertBeforeTerminator(unique_ptr<Instruction>(add));
+            hashVal = add;
+        }
+        if (args.size() >= 3)
+        {
+            auto *mult65 = new ConstantInt(IntegerType::getInstance(), 65);
+            auto *arg2 = asI32(args[2], bb, "memo_arg2_i32");
+            auto *mul = new BinaryOperator(Opcode::Mul, arg2, mult65, freshName("memo_hash_mul65"));
+            bb->insertBeforeTerminator(unique_ptr<Instruction>(mul));
+            auto *add = new BinaryOperator(Opcode::Add, hashVal, mul, freshName("memo_hash_add2"));
+            bb->insertBeforeTerminator(unique_ptr<Instruction>(add));
+            hashVal = add;
+        }
+    }
+
+    auto *cacheMask = new ConstantInt(IntegerType::getInstance(), kCacheSize - 1);
+    auto *cacheSlot = new BinaryOperator(Opcode::And, hashVal, cacheMask, freshName("memo_cache_slot"));
+    bb->insertBeforeTerminator(unique_ptr<Instruction>(cacheSlot));
+    return cacheSlot;
+}
+
 } // namespace
 
 bool MemoizationV2Pass::analyzeFunctionForMemoization(Function *func)
@@ -168,6 +481,58 @@ bool MemoizationV2Pass::analyzeFunctionForMemoization(Function *func)
         return false;
 
     return true;
+}
+
+MemoizationV2Pass::DirectIndex2ArgPlan MemoizationV2Pass::analyzeDirectIndex2Arg(Function *func) const
+{
+    DirectIndex2ArgPlan plan;
+
+    const auto &args = func->getArguments();
+    if (args.size() != 2)
+        return plan;
+
+    for (const auto &argPtr : args)
+    {
+        if (!argPtr->getType()->isIntegerTy())
+            return plan;
+    }
+
+    if (!recursiveArgsShrinkOnly(func))
+        return plan;
+
+    Argument *arg0 = args[0].get();
+
+    int max0 = inferArgUpperBound(func, 0);
+    const int fromArray = inferArgUpperBoundFromArrayIndex(func, 0);
+    if (fromArray > max0)
+        max0 = fromArray;
+
+    int max1 = inferArgUpperBound(func, 1);
+    const int fromCalls = inferArgUpperBoundFromCallSites(func->getParent(), func, 1);
+    if (fromCalls > max1)
+        max1 = fromCalls;
+
+    // Knapsack-like: two global arrays indexed by (arg0-1); derive stride from arg0 bound.
+    if (max0 >= 0 && max1 < 0 && hasKnapsackLikeArrayPattern(func, arg0))
+    {
+        const int stride = (CACHE_SIZE - 1) / (max0 + 1);
+        if (stride >= 2)
+        {
+            max1 = stride - 1;
+        }
+    }
+
+    if (max0 < 0 || max1 < 0)
+        return plan;
+
+    const int stride = max1 + 1;
+    const int64_t maxSlot = static_cast<int64_t>(max0) * stride + max1;
+    if (maxSlot >= CACHE_SIZE)
+        return plan;
+
+    plan.useDirectIndex = true;
+    plan.stride = stride;
+    return plan;
 }
 
 void MemoizationV2Pass::addMemoizationToFunction(Function *func)
@@ -254,7 +619,6 @@ void MemoizationV2Pass::addMemoizationToFunction(Function *func)
 
     auto *zero = new ConstantInt(IntegerType::getInstance(), 0);
     auto *one = new ConstantInt(IntegerType::getInstance(), 1);
-    auto *cacheMask = new ConstantInt(IntegerType::getInstance(), CACHE_SIZE - 1);
     auto *entrySizeConst = new ConstantInt(IntegerType::getInstance(), entrySize);
 
     vector<Value *> args;
@@ -262,31 +626,9 @@ void MemoizationV2Pass::addMemoizationToFunction(Function *func)
     for (const auto &argPtr : argPtrs)
         args.push_back(argPtr.get());
 
-    // hash = arg0 + arg1*33 + arg2*65
-    Value *hashVal = asI32(args[0], cacheLookupBlock, "memo_arg0_i32");
-    if (args.size() >= 2)
-    {
-        auto *mult33 = new ConstantInt(IntegerType::getInstance(), 33);
-        auto *arg1 = asI32(args[1], cacheLookupBlock, "memo_arg1_i32");
-        auto *mul = new BinaryOperator(Opcode::Mul, arg1, mult33, freshName("memo_hash_mul33"));
-        cacheLookupBlock->insertBeforeTerminator(unique_ptr<Instruction>(mul));
-        auto *add = new BinaryOperator(Opcode::Add, hashVal, mul, freshName("memo_hash_add1"));
-        cacheLookupBlock->insertBeforeTerminator(unique_ptr<Instruction>(add));
-        hashVal = add;
-    }
-    if (args.size() >= 3)
-    {
-        auto *mult65 = new ConstantInt(IntegerType::getInstance(), 65);
-        auto *arg2 = asI32(args[2], cacheLookupBlock, "memo_arg2_i32");
-        auto *mul = new BinaryOperator(Opcode::Mul, arg2, mult65, freshName("memo_hash_mul65"));
-        cacheLookupBlock->insertBeforeTerminator(unique_ptr<Instruction>(mul));
-        auto *add = new BinaryOperator(Opcode::Add, hashVal, mul, freshName("memo_hash_add2"));
-        cacheLookupBlock->insertBeforeTerminator(unique_ptr<Instruction>(add));
-        hashVal = add;
-    }
-
-    auto *cacheSlot = new BinaryOperator(Opcode::And, hashVal, cacheMask, freshName("memo_cache_slot"));
-    cacheLookupBlock->insertBeforeTerminator(unique_ptr<Instruction>(cacheSlot));
+    const DirectIndex2ArgPlan directPlan = analyzeDirectIndex2Arg(func);
+    Value *cacheSlot =
+        buildCacheSlotIndex(cacheLookupBlock, args, directPlan.useDirectIndex, directPlan.stride);
 
     auto *cacheIndex = new BinaryOperator(Opcode::Mul, cacheSlot, entrySizeConst, freshName("memo_cache_index"));
     cacheLookupBlock->insertBeforeTerminator(unique_ptr<Instruction>(cacheIndex));
@@ -429,7 +771,12 @@ void MemoizationV2Pass::addMemoizationToFunction(Function *func)
     if (verbose)
     {
         debugInfo << "MemoizationV2Pass: memoized " << funcName << " (slots=" << CACHE_SIZE
-                  << ", bytes=" << (totalSize * 4) << ")\n";
+                  << ", bytes=" << (totalSize * 4);
+        if (directPlan.useDirectIndex)
+            debugInfo << ", hash=direct_arg0*" << directPlan.stride << "+arg1";
+        else
+            debugInfo << ", hash=poly";
+        debugInfo << ")\n";
     }
 }
 
--- myCompiler/src/midend/pass/MemoizationV2Pass.h
+++ myCompiler/src/midend/pass/MemoizationV2Pass.h
@@ -13,8 +13,16 @@ namespace optimization
 
     private:
         static constexpr int MIN_RECURSIVE_CALLS = 2;
-        static constexpr int CACHE_SIZE = 4096; // 2^12 slots
+        static constexpr int CACHE_SIZE = 8192; // 2^13 slots
+
+        struct DirectIndex2ArgPlan
+        {
+            bool useDirectIndex = false;
+            int stride = 0; // slot = arg0 * stride + arg1
+        };
+
         bool analyzeFunctionForMemoization(Function *func);
+        DirectIndex2ArgPlan analyzeDirectIndex2Arg(Function *func) const;
         void addMemoizationToFunction(Function *func);
     };
 }
